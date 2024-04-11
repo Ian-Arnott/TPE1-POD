50ea7fb3a38e756cda8a6cfe8e9de2f7808a1af7
IanArnott <iarnott@itba.edu.ar>
2024-04-11 20:34:05 -0300

    adds compile script

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+server_dir="server/target"
+client_dir="client/target"
+tmp="tmp"
+
+mvn clean install
+mkdir -p "$tmp"
+
+cp "$server_dir/tpe1-g8-server-2024.1Q-bin.tar.gz" "$tmp/"
+cp "$client_dir/tpe1-g8-client-2024.1Q-bin.tar.gz" "$tmp/"
+cd "$tmp"
+
+tar -xzf "tpe1-g8-server-2024.1Q-bin.tar.gz"
+chmod +x tpe1-g8-server-2024.1Q/run-server
+sed -i -e 's/\r$//' tpe1-g8-server-2024.1Q/*.sh
+rm "tpe1-g8-server-2024.1Q-bin.tar.gz"
+
+
+tar -xzf "tpe1-g8-client-2024.1Q-bin.tar.gz"
+chmod +x tpe1-g8-client-2024.1Q/*Client
+sed -i -e 's/\r$//' tpe1-g8-client-2024.1Q/*.sh
+rm "tpe1-g8-client-2024.1Q-bin.tar.gz"
